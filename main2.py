a39d9a0c29c6f88066fdd5caf76f594e27fb3e9e
Anshuman <amedhi@connect.ust.hk>
2021-04-10 17:26:01 +0800

    simplify code to test deploy

--- main2.py
+++ main2.py
@@ -22,6 +22,10 @@ classes_recog = None
 agnostic_nms = False
 
 
+# Global initialization might be cached
+init()  # Initialize LPR
+
+
 def run_lpr(event, context):
     """Triggered by a change to a Cloud Storage bucket.
     Args:
@@ -33,12 +37,28 @@ def run_lpr(event, context):
 
     client = storage.Client()
     source_bucket = client.get_bucket(file["bucket"])
-    source_blob = source_bucket.get_blob(filename)
+    source_blob = source_bucket.get_blob(file["name"])
+
+    # Decode
+    image = np.asarray(bytearray(source_blob.download_as_string()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_UNCHANGED)
+
+    # Letterbox
+    img = letterbox(image, new_shape=imgsz_detect)[0]
+    # Stack
+    img = np.stack(img, 0)
+    # Convert
+    img = img[:, :, :, ::-1].transpose(0, 3, 1, 2)  # BGR to RGB, to bsx3x416x416
+    img = np.ascontiguousarray(img)
+
+    im0s = image  # source size image
+
+    res = proc(img, im0s, view_img=False)
 
     with NamedTemporaryFile() as temp:
-        print("Hello World", file=temp)
+        print(json.dumps(res), file=temp)
 
-        dest_filename = filename
+        dest_filename = file["name"]
         dest_bucket_name = "yolov5-output"
         dest_bucket = client.get_bucket(dest_bucket_name)
         dest_blob = dest_bucket.blob(dest_filename)
